7a5df22403c24b0136577eb5f172f17cdd1044c0
MarieJF <marie.jacquemin-fanti@student.hepl.be>
2026-02-24 11:22:10 +0100

    fix: make table available

--- db/connexion.php
+++ db/connexion.php
@@ -1,4 +1,10 @@
 <?php
+require_once __DIR__.'/../vendor/autoload.php';
+if (!isset($dotenv)){
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+    $dotenv->load();
+}
+
 $connexion = $_ENV['db_connection'];
 $host = $_ENV['db_host'];
 $db_name = $_ENV['db_database'];
